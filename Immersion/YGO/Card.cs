7cbcd0fc2abef42e9f1b4928aaad53aa9855cfab
Hugo Schlegel <79219993+Happatsu@users.noreply.github.com>
2021-06-15 21:16:57 +0200

    Initial Commit

---
+++ Immersion/YGO/Card.cs
@@ -0,0 +1,14 @@
+namespace YGO
+{
+    public class Card
+    {
+        public TYPE type;
+
+        public enum TYPE
+        {
+            MONSTER,
+            TRAP,
+            MAGIC
+        }
+    }
+}
