7bb28824a5c18bd38a55da07e08924a4bbfcaf12
Hugo Schlegel <hugo.schlegel@epita.fr>
2021-08-02 18:35:00 +0200

    added some bs to the code

--- Immersion/Basics/Basics.cs
+++ Immersion/Basics/Basics.cs
@@ -2,5 +2,28 @@ using System;
 
 namespace Basics
 {
-    
+    class Basics 
+    {
+        public static void HelloWho(string str)
+        {
+            Console.WriteLine("Hello" + str + "!");
+        }
+
+        public static int ABS(int n)
+        {
+            if (n > 0)
+                return n;
+            return -1 * n;
+        }
+        
+        public static int Fibonacci(int n)
+        {
+            int F0 = 0, F1 = 1;
+            for (var i = 0; i < n; i++)
+            {
+                F0 = F1; F1 = F0 + F1;
+            }
+            return F0;
+        }
+    }
 }
